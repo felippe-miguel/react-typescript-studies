3b6a403205bf874bc53a9ff7a40e173e14dbe68b
Felippe Miguel <felippe.miguel.silva@gmail.com>
2025-09-21 13:38:24 +0000

    feat: add Link component with styling and integrate into App

--- luz-e-cena/src/App.tsx
+++ luz-e-cena/src/App.tsx
@@ -1,7 +1,15 @@
 import Banner from "./components/banner";
+import Link from "./components/Link";
 
 function App() {
-  return <Banner src="./images/Banner.png" alt="Banner principal da página" />;
+  return (
+    <>
+      <Banner src="./images/Banner.png" alt="Banner principal da página" />
+      <Link href="/" target="_blank">
+        Ir para Alura
+      </Link>
+    </>
+  );
 }
 
 export default App;
