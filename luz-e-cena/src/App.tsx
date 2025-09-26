925dc5d5e8d08ff9311ed7bd3c0e204c7dd1e930
Felippe Miguel <felippe.miguel.silva@gmail.com>
2025-09-26 13:30:23 +0000

    feat: include MovieSection component in App

--- luz-e-cena/src/App.tsx
+++ luz-e-cena/src/App.tsx
@@ -1,11 +1,13 @@
 import Banner from "./components/Banner";
 import Header from "./components/Header";
+import MovieSection from "./components/MovieSection";
 
 function App() {
   return (
     <>
       <Header />
       <Banner src="./images/Banner.png" alt="Banner principal da página" />
+      <MovieSection />
     </>
   );
 }
