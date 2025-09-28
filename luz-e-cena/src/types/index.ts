66385f066b00db32b9e075e9e1cc87190fa625f3
Felippe Miguel <felippe.miguel.silva@gmail.com>
2025-09-28 18:09:43 +0000

    feat: enhance MovieSection with styling and movie data integration

--- luz-e-cena/src/types/index.ts
+++ luz-e-cena/src/types/index.ts
@@ -2,11 +2,12 @@ export type Categoria = "2D" | "3D";
 export type Censura = "Livre" | "10 anos" | "12 anos" | "14 anos" | "16 anos";
 
 export interface MovieCardProps {
+  id: number;
   src: string;
   alt: string;
   titulo: string;
   genero: string;
-  duracao: string;
+  duracao: number;
   categoria: Categoria;
   censura: Censura;
 }
